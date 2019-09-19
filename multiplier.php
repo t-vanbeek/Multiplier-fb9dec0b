32bb4645a53c9103dad33f846fe1d64561008b1c
Thorvald van Beek <54794887+t-vanbeek@users.noreply.github.com>
2019-09-19 12:37:27 +0200

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,8 @@
+<?php
+echo "> Ik ga twee getallen met elkaar vermenigvuldigen".PHP_EOL;
+$getal1 = readline ("> Eerste getal?".PHP_EOL);
+$getal2 = readline ("> Tweede getal?".PHP_EOL);
+
+$antwoord = $getal1 * $getal2;
+
+echo "> Het antwoord is: " .$antwoord;
